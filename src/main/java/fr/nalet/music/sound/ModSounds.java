b8cc6a8f26c99f74f07e9ae6a55bf7532bc1b15a
MrBread13 <valentin.bonsi2@gmail.com>
2022-10-11 17:48:34 +0200

    Added 4 songs
    -Rick roll
    -On m'appelle l'ovni
    -Ain't Talking About love
    -Owner of a lonely heart

--- src/main/java/fr/nalet/music/sound/ModSounds.java
+++ src/main/java/fr/nalet/music/sound/ModSounds.java
@@ -9,15 +9,26 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModSounds {
-    public static final DeferredRegister<SoundEvent> SOUND_EVENT =
+        public static final DeferredRegister<SoundEvent> SOUND_EVENT =
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Music.MOD_ID);
 
-    public static final RegistryObject<SoundEvent> RAMBLE_ON =
+        public static final RegistryObject<SoundEvent> RAMBLE_ON =
             registerSoundEvent("ramble_on");
 
-    public static final RegistryObject<SoundEvent> REAL_GONE =
+        public static final RegistryObject<SoundEvent> REAL_GONE =
             registerSoundEvent("real_gone");
 
+        public static final RegistryObject<SoundEvent> OWNER_OF_A_LONELY_HEART =
+            registerSoundEvent("owner_of_a_lonely_heart");
+        
+        public static final RegistryObject<SoundEvent> OVNI =
+                registerSoundEvent("ovni");
+
+        public static final RegistryObject<SoundEvent> ABOUT_LOVE =
+                registerSoundEvent("about_love");
+
+        public static final RegistryObject<SoundEvent> RICK_ROLL =
+                registerSoundEvent("rick_roll");
 
 
 
